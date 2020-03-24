f465dba09197370f4466ecf4ce7d9e4bca2be879
Willington Vega <wvega@wvega.com>
2020-03-24 10:04:17 -0500

    Replace username with password in template question

--- src/Codeception/Template/Lumiere.php
+++ src/Codeception/Template/Lumiere.php
@@ -135,7 +135,7 @@ class Lumiere extends Wpbrowser {
 		);
 
 		$installation_data['ACCEPTANCE_DB_PASSWORD'] = $this->ask(
-			'What is the username for the database you\'ll use for acceptance tests?',
+			'What is the password for the database you\'ll use for acceptance tests?',
 			'root'
 		);
 
@@ -160,7 +160,7 @@ class Lumiere extends Wpbrowser {
 		);
 
 		$installation_data['INTEGRATION_DB_PASSWORD'] = $this->ask(
-			'What is the username for the database you\'ll use for integration tests?',
+			'What is the password for the database you\'ll use for integration tests?',
 			'root'
 		);
 
